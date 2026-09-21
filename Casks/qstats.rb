3844a4255792c8c2ac52590c502be14dec5cc514
qghs <1556448918@qq.com>
2026-09-21 18:58:32 +0800

    feat: initial Cask for Qstats v1.2.0

---
+++ Casks/qstats.rb
@@ -0,0 +1,34 @@
+cask "qstats" do
+  arch arm: "apple-silicon", intel: "intel"
+
+  version "1.2.0"
+  sha256 arm:   "32aa8779a45ffbe743c594c1acc0846afddb28266a14e86f0634eb0236f2f39a",
+         intel: "418faacfd1ab222dc7a9d6df428d28f896bfaae49b2e099464f2263670297e65"
+
+  url "https://github.com/qg-hs/Qstats/releases/download/v#{version}/Qstats-#{version}-#{arch}.dmg"
+  name "Qstats"
+  desc "Lightweight macOS menu bar system monitor and screenshot tool"
+  homepage "https://github.com/qg-hs/Qstats"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :ventura"
+
+  app "Qstats.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-rd", "com.apple.quarantine", "#{appdir}/Qstats.app"],
+                   sudo: false
+  end
+
+  uninstall quit: "com.qghs.Qstats"
+
+  zap trash: [
+    "~/Library/Preferences/com.qghs.Qstats.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,28 @@
+# Homebrew Tap for Qstats
+
+Official Homebrew Tap for [Qstats](https://github.com/qg-hs/Qstats).
+
+## 安装 / Installation
+
+```bash
+brew install --cask qg-hs/tap/qstats
+```
+
+或者添加 Tap 后安装：
+
+```bash
+brew tap qg-hs/tap
+brew install --cask qstats
+```
+
+## 更新 / Upgrade
+
+```bash
+brew upgrade --cask qstats
+```
+
+## 卸载 / Uninstall
+
+```bash
+brew uninstall --cask qstats
+```
